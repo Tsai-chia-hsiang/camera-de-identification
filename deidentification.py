a2cb6e2aefc6a23dde4cf5daf994fc44d2518515
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2024-03-25 13:15:18 +0800

    adding yml

--- deidentification.py
+++ deidentification.py
@@ -1,6 +1,8 @@
 import os
 import os.path as osp
 import os
+import argparse
+import yaml
 import warnings
 warnings.filterwarnings("ignore")
 import logging
@@ -21,25 +23,43 @@ def write_batches(deid_img_buf, batchsize):
             MAX_Thread_num = batchsize
         )
 
+def get_ymal_config(config_file:os.PathLike)->dict:
+    with open(config_file, "r") as file:
+        yaml_data = yaml.safe_load(file)
+    return yaml_data
+
+def parse_arguments()-> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", 
+        default=osp.join(".", "config.yml"), type=str, 
+        help="your config (.yml) file path"
+    )
+    return parser.parse_args()
+
 if __name__ == "__main__":
 
-    device="cuda:1"
+    args = parse_arguments()
+    config = get_ymal_config(args.config)
 
-    batchsize = 18
-    bufsize = 10
+    print(f"source images root : {config['srcroot']}, save to : {config['dstroot']}")
+    print(f"batch size : {config['batchsize']}; bufisze : {config['bufsize']}")
 
-    src_img_root = osp.join("0_1015/")
-    deid_img_root = build_file_tree(src_img_root, osp.join("de_identification"))
+    deid_img_root = build_file_tree(config["srcroot"], config["dstroot"])
 
-    dataset = DeID_Dataset(img_paths=all_files(src_img_root, only_want_ftype=".jpg"))
-    loader = DataLoader(dataset=dataset, batch_size=batchsize)
+    dataset = DeID_Dataset(img_paths=all_files(config["srcroot"], only_want_ftype=".jpg"))
+    loader = DataLoader(dataset=dataset, batch_size=config["batchsize"])
     
     license_plate_seg = License_Plate_Segmentor(
-        deID_modelpath=osp.join("model","license_plate_seg.pth"),
-        device=device, thr=5e-2
+        deID_modelpath=config["license_plate_model"]["modelpath"],
+        device=config["device"], 
+        thr=config["license_plate_model"]["threshold"]
     )
-    human_detector = YOLO_NAS_Human_Detector(yolonas_type="yolo_nas_l", device=device)
-    deid_img_buf = [None]*bufsize
+    human_detector = YOLO_NAS_Human_Detector(
+        yolonas_type=config["yolo_nas"], 
+        device=config["device"]
+    )
+    deid_img_buf = [None]*config["bufsize"]
     
     clear_chached_buf = False
     pbar = tqdm(loader)
@@ -49,32 +69,35 @@ if __name__ == "__main__":
         
         clear_chached_buf = False
 
-        l=license_plate_seg(batch_img=norimg.to(torch.device(device)))
+        l=license_plate_seg(batch_img=norimg.to(torch.device(config["device"])))
         pbar.set_postfix(ordered_dict = {"stage":"license"})
         h = human_detector(batch_img = list(pth), verbose=False)
         pbar.set_postfix(ordered_dict = {"stage":"human"})
             
         deid_img = mask_batch_tensor_image(
-            img = oriimg.to(device=torch.device(device)), 
+            img = oriimg.to(device=torch.device(config["device"])), 
             mask = l*h, return_np_img = True
         )
         
         to_files = list(
-            osp.join(deid_img_root, pth_i.removeprefix(src_img_root))
+            osp.join(deid_img_root, pth_i.removeprefix(config["srcroot"]))
             for pth_i in list(pth)
         )
         
-        deid_img_buf[i%bufsize] = (deid_img, to_files)
+        deid_img_buf[i%config["bufsize"]] = (deid_img, to_files)
         
-        if (i+1) % bufsize == 0:
-            pbar.set_postfix(ordered_dict = {"stage":f"write batch"})
-            write_batches(deid_img_buf = deid_img_buf, batchsize = batchsize)
+        if (i+1) % config["bufsize"] == 0:
+            pbar.set_postfix(ordered_dict = {"stage":f"write batches"})
+            write_batches(
+                deid_img_buf = deid_img_buf, 
+                batchsize = config["batchsize"]
+            )
             clear_chached_buf = True
 
     if not clear_chached_buf:
         write_batches(
-            deid_img_buf = deid_img_buf[0:i%bufsize+1], 
-            batchsize = batchsize
+            deid_img_buf = deid_img_buf[0:i%config["bufsize"]+1], 
+            batchsize = config["batchsize"]
         )
         
-    os.system("rm -rf ~/sg_logs/*")   
+    os.system("rm -rf ~/sg_logs/*") 
